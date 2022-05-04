c034208c46e3ba4b4b8c86713e1a770a68850885
MichalHorejs <47171877+MichalHorejs@users.noreply.github.com>
2022-05-05 00:29:56 +0200

    Main class comment

--- src/main/java/application/ija/Main.java
+++ src/main/java/application/ija/Main.java
@@ -7,6 +7,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Main function starting application
+ */
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
